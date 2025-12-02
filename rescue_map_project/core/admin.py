f3c96bde5d3bcc81a1b9ac76357a04e40d909fd1
Lam <lamlv723@gmail.com>
2025-12-03 02:11:18 +0700

    register to admin

--- rescue_map_project/core/admin.py
+++ rescue_map_project/core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, RescueTeam
 
-# Register your models here.
+# Đăng ký User với giao diện mặc định + field mới
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Extra Info', {'fields': ('role', 'phone_number', 'team')}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(RescueTeam)
