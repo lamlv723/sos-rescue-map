242315831c5f29617fe7e89378dc5469153ce8b3
Lam <lamlv723@gmail.com>
2025-12-03 01:35:20 +0700

    step 2: create django user table

--- rescue_map_project/core/models.py
+++ rescue_map_project/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    # New fields added to the default User model
+    role = models.CharField(max_length=50, default='Citizen')
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    team_id = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'core_user'
