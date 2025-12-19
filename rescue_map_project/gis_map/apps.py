6d09c107f15c572174945317953b30033fa25be6
Phan Vu <phanvubuondon@gmail.com>
2025-12-19 09:15:51 +0700

    Feature/vu a data engineer (#10)
    * chore: init feature branch for Part A (Data Engineer)
    
    * feat(models): define SOS domain models and GIS Region/MapPoint
    
    * Chore: add mock data (json) and scripts to generate geo_mock, load mock_data to PostgreSQL
    
    * Chore: add test, readme, requirement (update) and gdal wheel for PostgreSQL
    
    * Chore: setting for backend and database
    
    * Chore: auto create map_point based on sos_request

--- rescue_map_project/gis_map/apps.py
+++ rescue_map_project/gis_map/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
+class MapsConfig(AppConfig):
+    name = "gis_map"
+    def ready(self):
+        import gis_map.signals
 
-class GisMapConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gis_map'
