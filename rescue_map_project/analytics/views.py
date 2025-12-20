5ceb5bf3c246db37b9d65a49f4d4f9265c9a38c8
Lam <lamlv723@gmail.com>
2025-12-20 14:05:08 +0700

    minor fix views.py

--- rescue_map_project/analytics/views.py
+++ rescue_map_project/analytics/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
 def dashboard(request):
-    mockdata = fetch ("dường dẫn tới file dashboard_mocdata.json") # thay thế ghi ghép hoàn chỉnh
+    # mockdata = fetch ("dường dẫn tới file dashboard_mocdata.json") # thay thế ghi ghép hoàn chỉnh
     # dựa theo CRUD
 
-    total_request = mockdata_getallrequest;
+    # total_request = mockdata_getallrequest;
 
-    return render(request, total_request, 'analytics/dashboard.html')
+    # return render(request, total_request, 'analytics/dashboard.html')
+    return render(request, 'analytics/dashboard.html')
